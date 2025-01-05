35e23ca58daceb9f791ff01b6d6a324d088744c4
Bernd Malle <bernd.malle@gmail.com>
2025-01-05 14:54:24 +0100

    useEffect() works with manually resolving the fetch response & the json, so apparently I misunderstood PBs .getList() / .getFullList() return type

--- app/routes/chats.tsx
+++ app/routes/chats.tsx
@@ -3,6 +3,7 @@ import { createServerFn } from "@tanstack/start";
 import { getPb } from "@/db/pb";
 import { useCallback, useEffect, useState } from "react";
 import { ChatsResponse } from "@/types/pocketbase-types";
+import { ListResult } from "pocketbase";
 
 export const Route = createFileRoute("/chats")({
   component: PocketChats,
@@ -10,18 +11,20 @@ export const Route = createFileRoute("/chats")({
 });
 
 export default function PocketChats() {
-  const [chats, setChats] = useState<any[]>([]);
-  const [isInitialRender, setIsInitialRender] = useState(true);
+  const [chats, setChats] = useState<ChatsResponse[]>([]);
 
   const getChatWithUsers = useCallback(async () => {
-    const chats = await getPb().collection("chats").getList();
-    // .getFullList<ChatsResponse>(200, {
+    // const chatResponse = await getPb().collection("chats").getFullList<ChatsResponse>(200, {
     //   sort: "created",
     //   expand: "users",
     //   // filter: `users ~ '${$currentUser.id}'`,
     // });
-    setIsInitialRender(false);
-    // setChats(chats.items);
+
+    const response = await fetch("http://localhost:2511/api/collections/chats/records");
+    const chatResponse = (await response.json()) as ListResult<ChatsResponse>;
+    console.log({ chatResponse });
+
+    setChats(chatResponse.items);
   }, []);
 
   useEffect(() => {
@@ -30,11 +33,24 @@ export default function PocketChats() {
     // return () => {
     //   getPb().collection("chats").unsubscribe();
     // };
-  }, [getChatWithUsers, isInitialRender]);
+  }, []);
 
   return (
     <div className="pb-chats p-4">
       <h1 className="text-3xl text-primary">Chats</h1>
+
+      <div className="grid grid-cols-3 gap-4">
+        {chats.map((chat) => (
+          <div key={chat.id} className="mt-2 border p-4 rounded-xl">
+            <h2 className="text-xl text-secondary">{chat.title}</h2>
+            <div className="text-sm text-gray-500">
+              {chat.users.map((user) => (
+                <div key={user}>{user}</div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
